f2d900340880c241d384d47ddc5ff7e7c08ee2f7
Viniczay Kata <119952403+kataviniczay@users.noreply.github.com>
2025-11-20 13:51:03 +0100

    Add files via upload

---
+++ Lakcim.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Projektkonzol
+{
+	public class Lakcim
+	{
+		public string Iranyitoszam { get; set; }
+		public string Varos { get; set; }
+		public string UtcaHazszam { get; set; }
+
+		public Lakcim(string[] tomb)
+		{
+			Iranyitoszam = tomb[0];
+			Varos = tomb[1];
+			UtcaHazszam = tomb[2];
+		}
+	}
+}
+
