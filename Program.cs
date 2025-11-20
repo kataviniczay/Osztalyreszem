50c27ac2a18e1e00ed853bfda284e36832a9f414
Creator73Z <160496256+Creator73Z@users.noreply.github.com>
2025-11-20 11:55:55 +0100

    Anyád

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -95,7 +95,7 @@ namespace Projektkonzol
             diakok.Add(new Felkeszito(t));
 
             StreamWriter txt = new StreamWriter("Felkészítő.txt", true);
-            txt.WriteLine($"{t[0]};{t[1]};{t[2]};{t[3]};{t[4]};{t[5]};{t[6]};{t[7]};{t[8]};{t[9]};{t[10]};{t[11]}");
+            txt.WriteLine($"\b{t[0]};{t[1]};{t[2]};{t[3]};{t[4]};{t[5]};{t[6]};{t[7]};{t[8]};{t[9]};{t[10]};{t[11]}");
             txt.Close();
         }
     }
